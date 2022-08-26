f00c20907a78184b547a017d81880de9dacebd16
raihaan <patelraihaan@gmail.com>
2022-08-26 13:50:55 +0100

    updated for new data

--- scripts/12_include_vs_exclude.R
+++ scripts/12_include_vs_exclude.R
@@ -9,9 +9,13 @@ load("../../raw_data/df_ukb_raw.rda")
 rawdata_demo<-df_ukb_raw[,c("eid","Age_when_attended_assesment_centre_0_0","Sex","education_years")]
 rm(df_ukb_raw)
 
+#subset to 50+
+rawdata_demo<-subset(rawdata_demo, rawdata_demo$Age_when_attended_assesment_centre_0_0>=50)
+#n=384642
+
 #get analysis sample
-load("../../raw_data/train_data_outliers_removed.rda")
-load("../../raw_data/test_data_outliers_removed.rda")
+load("../../raw_data/train_data_outliers_removed_fiftyplusnoapoe.rda")
+load("../../raw_data/test_data_outliers_removed_fiftyplusnoapoe.rda")
 df_analysis<-rbind(train.data,test.data)
 rm(train.data, test.data)
 
@@ -31,36 +35,36 @@ df_merged$dataset[is.na(df_merged$dataset)]<-"excluded"
 df_merged$dataset<-as.factor(df_merged$dataset)
 summary(df_merged$dataset)
 #excluded included 
-#294592   207929 
+#163880   220762 
 #cool
 
 excluded_sample <- subset(df_merged, df_merged$dataset=="excluded")
 included_sample <- subset(df_merged, df_merged$dataset=="included")
 age_test<-t.test(Age_when_attended_assesment_centre_0_0 ~ dataset, data = df_merged, var.equal = FALSE)
 #data:  Age_when_attended_assesment_centre_0_0 by dataset
-#t = 19.702, df = 445079, p-value < 2.2e-16
+#t = 13.31, df = 352022, p-value < 2.2e-16
 #alternative hypothesis: true difference in means is not equal to 0
 #95 percent confidence interval:
-#  0.4118751 0.5028760
+#  0.2017481 0.2714272
 #sample estimates:
 #  mean in group excluded mean in group included 
-#56.71774               56.26036 
-sd(excluded_sample$Age_when_attended_assesment_centre_0_0) #8.06
-sd(included_sample$Age_when_attended_assesment_centre_0_0) #8.14
+#60.21101               59.97442 
+sd(excluded_sample$Age_when_attended_assesment_centre_0_0) #5.46
+sd(included_sample$Age_when_attended_assesment_centre_0_0) #5.44
 
 
 edu_test<-t.test(education_years ~ dataset, data = df_merged, var.equal = FALSE)
 edu_test
 #data:  education_years by dataset
-#t = -56.422, df = 451094, p-value < 2.2e-16
+#t = -55.771, df = 339144, p-value < 2.2e-16
 #alternative hypothesis: true difference in means is not equal to 0
 #95 percent confidence interval:
-#  -0.5127296 -0.4783032
+#  -0.5825166 -0.5429639
 #sample estimates:
 #  mean in group excluded mean in group included 
-#13.30334               13.79886 
+#12.98370               13.54644  
 sd(excluded_sample$education_years, na.rm=TRUE) #3.07
-sd(included_sample$education_years) #3.03
+sd(included_sample$education_years) #3.05
 
 df_merged$Sex<-as.factor(df_merged$Sex)
 sex_test<-chisq.test(df_merged$dataset, df_merged$Sex, correct=FALSE)
@@ -68,10 +72,10 @@ sex_test
 #Pearson's Chi-squared test
 #
 #data:  df_merged$dataset and df_merged$Sex
-#X-squared = 653.94, df = 1, p-value < 2.2e-16
+#X-squared = 1819.4, df = 1, p-value < 2.2e-16
 summary(as.factor(excluded_sample$Sex))
 #0      1 
-#164718 129874 
+#95444 68436 
 summary(as.factor(included_sample$Sex))
 #0      1 
-#108676  99253 
+#113276 107486 
